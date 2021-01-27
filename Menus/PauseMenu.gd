776620f5e2c86dd1b95ae935485da62cae3ecfb8
MrBundles <45633671+MrBundles@users.noreply.github.com>
2021-01-27 15:56:39 -0500

    Added all remaining SFX

--- Menus/PauseMenu.gd
+++ Menus/PauseMenu.gd
@@ -6,5 +6,9 @@ func _ready():
 	GlobalSignalManager.connect("resume_scene", self, "_on_resume_scene")
 
 
+func _process(delta):
+	$CanvasLayer.offset = rect_global_position
+
+
 func _on_resume_scene():
 	$CanvasLayer/ColorRect.hide()
